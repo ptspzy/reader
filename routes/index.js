783bba9f47583e76d38ea3a8fa15e02cf2be49a3
pengzhongyao <pengzhongyao@xingshulin.com>
2017-07-03 19:23:03 +0800

    Temp push

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs')
 var ebook = path.join(__dirname, "../public/ebook");
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     var fs = require('fs');
-    fs.readdir(ebook, function (err, names) {    //names为目录下的文件数组列表
+    fs.readdir(ebook, function(err, names) { //names为目录下的文件数组列表
         if (err) {
             return console.log(err);
-        }
-        else {
-            res.render('index', {title: 'Reader', book: names});
+        } else {
+            res.render('index', { title: 'Reader', book: names });
         }
     });
 });
 
-router.get('book/:filename', function (req, res, next) {
+router.get('/book/:filename', function(req, res, next) {
 
     var fileName = req.params.filename;
     var filePath = path.join(ebook, '/1.txt')
 
     console.log(filePath)
-
-    // fs.readFile(filePath, "utf-8", function (error, data) {
-    //     //console.log(error)
-    //     if (error) {
-    //         console.log(error)
-    //         return;
-    //     }
-    //     //console.log(data);
-    //     //return "<p>" + data.replace(/\r\n/g, "</p><p>") + "</p>";
-    //     //res.json("<p>" + data.replace(/\r\n/g, "</p><p>") + "</p>")
-    //     res.json("{'aa':'bb'}");
-    // });
+    fs.readFile(filePath, 'utf8', function(err, data) {
+        //console.log(error)
+        if (err) throw err;
+        console.log(data);
+        //return "<p>" + data.replace(/\r\n/g, "</p><p>") + "</p>";
+        res.send("<p>" + data.toString('utf8').replace(/\r\n/g, "</p><p>") + "</p>");
+        //res.send("{'aa':'bb'}");
+    });
 });
 
 module.exports = router;
